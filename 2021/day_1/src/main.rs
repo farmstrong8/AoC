a2080ab4f456f1b30d143a4fb16c0d923f2472ce
Ferris Armstrong <fda@bu.edu>
2022-03-23 19:02:23 -0400

    add answer to day 1 part 2

--- 2021/day_1/src/main.rs
+++ 2021/day_1/src/main.rs
@@ -20,7 +20,38 @@ fn part_1() {
 }
 
 fn part_2() {
+    let string_data = fs::read_to_string("src/input.txt").expect("file not found");
+    let split: Vec<&str> = string_data.split("\r\n").collect();
+    let mut increase_count = 0;
 
+    for (index, _) in split.iter().enumerate() {
+        let a1 = split.get(index);
+        let a2 = split.get(index + 1);
+        let a3 = split.get(index + 2);
+
+        let b1 = split.get(index + 1);
+        let b2 = split.get(index + 2);
+        let b3 = split.get(index + 3);
+
+        if a1.is_some() && a2.is_some() && a3.is_some() && b1.is_some() && b2.is_some() && b3.is_some() {
+            let c1 = a1.unwrap().parse::<i32>().unwrap();
+            let c2 = a2.unwrap().parse::<i32>().unwrap();
+            let c3 = a3.unwrap().parse::<i32>().unwrap();
+
+            let d1 = b1.unwrap().parse::<i32>().unwrap();
+            let d2 = b2.unwrap().parse::<i32>().unwrap();
+            let d3 = b3.unwrap().parse::<i32>().unwrap();
+
+            let window_1 = c1 + c2 + c3;
+            let window_2 = d1 + d2 + d3;
+
+            if window_2 > window_1 {
+                increase_count += 1;
+            }
+        }
+    }
+
+    println!("THIS IS INCREASE COUNT {}", increase_count);
 }
 
 fn main() {
--- 2021/day_1/target/debug/deps/day_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Decra\Documents\AoC\2021\day_1\target\debug\deps\day_1.exe: src\main.rs
-
-C:\Users\Decra\Documents\AoC\2021\day_1\target\debug\deps\day_1.d: src\main.rs
-
-src\main.rs:
